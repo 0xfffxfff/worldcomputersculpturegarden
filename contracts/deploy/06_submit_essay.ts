c0a37471bd62e1447e0d54db716c4b4538783b80
0xfff <0xfff@protonmail.com>
2024-11-11 14:12:07 +0100

    chore: update sepolia deps

--- contracts/deploy/06_submit_essay.ts
+++ contracts/deploy/06_submit_essay.ts
@@ -21,12 +21,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const tx1 = await essay.setTextPt1(essayText.textPt1, {
       from: deployer,
       gasLimit: 30_000_000,
-      gasPrice: 15000000000,
+      // gasPrice: 15000000000,
     });
     const tx2 = await essay.setTextPt2(essayText.textPt2, {
       from: deployer,
       gasLimit: 30_000_000,
-      gasPrice: 15000000000,
+      // gasPrice: 15000000000,
     });
     console.log(`Essay text set!`, tx1.hash, tx2.hash);
   }
@@ -39,7 +39,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const tx = await essay.setTitle(essayText.title, {
       from: deployer,
       gasLimit: 30_000_000,
-      gasPrice: 100000000000,
+      // gasPrice: 100000000000,
     });
     console.log(`Essay title set!`, tx.hash);
   }
